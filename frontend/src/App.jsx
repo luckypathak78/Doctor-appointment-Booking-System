79c61a4b4bcce80ca5d85c4d12bae661b189923d
weather app <lucky1945pathak@gmail.com>
2026-01-09 19:06:01 +0530

    Add different pages and create Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Myprofile from './pages/Myprofile'
+import MyAppointmnets from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/my-profile' element={<Myprofile />}/>
+        <Route path='/my-appointments' element={<MyAppointmnets />}/>
+        <Route path='/appointment/:docID' element={<Appointment />}/>
+      </Routes>
+     </div>
   )
 }
 
