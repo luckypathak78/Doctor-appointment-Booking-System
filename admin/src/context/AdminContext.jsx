bd00e8d9f2804aa03008259d2ce08f447cf56737
weather app <lucky1945pathak@gmail.com>
2026-02-13 21:58:43 +0530

    completed Admin dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,6 +11,7 @@ const AdminContextProvider = (props) => {
     );
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -121,6 +122,28 @@ const AdminContextProvider = (props) => {
   }
 }
 
+ const getDashData = async () => {
+  try {
+
+    const { data } = await axios.get(
+      backendUrl + '/api/admin/dashboard',
+      {
+        headers: {
+          Authorization: `Bearer ${aToken}`,
+        },})
+
+    if (data.success) {
+      setDashData(data.dashData)
+      console.log(data.dashData);
+    } else {
+      toast.error(data.message)
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
     const value = {
         aToken,
@@ -129,7 +152,8 @@ const AdminContextProvider = (props) => {
         getAllDoctors, changeAvailability,
         appointments,setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,getDashData
     };
 
     return (
